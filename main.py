fa603b1199919bf0b889be848ab33eec29b60f2e
RAF-coder-py <ralph.honhon@icloud.com>
2024-07-30 00:02:50 +0200

    1st commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import pygame as py
+from Jeu_1.game import Game
+
+
+
+width = 800
+height = 600
+background = py.image.load('pictures/background.png')
+background = py.transform.scale(background, (width + 500, height))
+clock = py.time.Clock
+
+
+
+
+def main():
+
+    running = True
+    game = Game()
+    rotation = -1
+    while running:
+        
+
+        for event in py.event.get():
+
+            if event.type == py.QUIT:
+                running = False
+
+            elif event.type == py.KEYDOWN:
+
+                if rotation == 1 and event.key == py.K_LEFT:
+                    game.player.turn()
+                    rotation = -1
+                    
+                elif rotation == 0 and event.key == py.K_RIGHT:
+                    game.player.turn()
+                    rotation = -1
+                elif event.key == py.K_SPACE:
+                    print(1)
+                    
+                    
+
+        keys = py.key.get_pressed()
+        
+        if keys[py.K_RIGHT]:
+            game.player_move_right()
+            rotation = 1
+
+        elif keys[py.K_LEFT]:
+            game.player_move_left()
+            rotation = 0
+
+    
+        game.screen.blit(background, (0, 0))
+        game.screen.blit(game.player.image,(game.player.rect.x,game.player.position_y))
+        py.display.set_caption('1st game')    
+        py.display.flip()
+
+    py.quit
+
+
+if __name__ == '__main__':
+    main()
